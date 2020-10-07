2f3b2486e5464d92f86b2dc00e5a65a50f808b15
yf116 <yf116@duke.edu>
2020-10-07 18:02:00 -0400

    uncomment

--- classification/train.py
+++ classification/train.py
@@ -16,7 +16,7 @@ def resnet_classification(loading_model=False, image_root='trainval', model_name
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=6,
                                                   shuffle=True, num_workers= multiprocessing.cpu_count() // 2)
                    for x in ['train', 'validation']}
-    class_names = image_datasets['train'].classes
+    # class_names = image_datasets['train'].classes
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'validation']}
 
